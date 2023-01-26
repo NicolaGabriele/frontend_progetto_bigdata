80875a097ed787ade0f5a57b252918049a2d5a4f
Nicola <nicolagabriele99@gmail.com>
2023-01-26 09:57:50 +0100

    risolto bug di navigazione tra le pagine

--- lib/Widgets/pages/QueriesPage.dart
+++ lib/Widgets/pages/QueriesPage.dart
@@ -13,9 +13,33 @@ class _QueriesPageState extends State<StatefulWidget>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Row(
+              children: [
+                Container(
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle
+                    ) ,
+                    child: Image.asset("images/logo.jpg")
+                ),
+                const Text(
+                  'Hotel Reviews Analitycs',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontStyle: FontStyle.italic
+                  ),
+                )
+              ],
+            )
+        ), //aggiungere bellezze
+      ),
         body: Row(
           children: [SubmitPage(),VerticalDivider(width: 2,color: Colors.black,),VisualizzationPage()],
-        )
+        ),
+      bottomNavigationBar: BottomAppBar(color: Colors.red, elevation: 10,child: Container(height: 25,),),
     );
   }
 
--- lib/Widgets/pages/SubmitPage.dart
+++ lib/Widgets/pages/SubmitPage.dart
@@ -9,15 +9,66 @@ class SubmitPage extends StatefulWidget{
 }
 
 class _SubmitPageState extends State<SubmitPage>{
+
+  Map<String,Form> visualMapping = {
+    'negative reviews word count':Form.WORD_COUNT_NEGATIVE,
+    'positive reviews word count': Form.WORD_COUNT_POSITIVE
+  };
+  Form? _form = Form.WORD_COUNT_POSITIVE;
   @override
   Widget build(BuildContext context) {
     //TODO
     return Container(
-      child: Padding(
-        padding: EdgeInsets.only(top: 200, bottom: 200,left: 200,right: 200),
-        child: Text("Area di Submit"),
-      ),
+      child: Stack(
+        children: [
+          Opacity(
+            opacity: 0.2,
+            child: Image.asset("/images/submit.jpg"),
+          ),
+          buildForm()
+        ],
+      )
     );
   }
 
+  Widget buildForm(){
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 50, top: 30),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.red.shade50,
+              ),
+              child: DropdownButton(
+                items: buildItems(),
+                onChanged: (String? value)=>setState(()=>_form = visualMapping[value]),
+                dropdownColor: Colors.red.shade50,
+                underline: Container(height: 2,color: Colors.red.shade700,),
+              ),
+            )
+        ),
+        //mettere form
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: ElevatedButton(
+              onPressed: ()=>{},
+              child: Text('submit')
+          ),
+        )
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> buildItems(){
+      return visualMapping.keys.map((e) => DropdownMenuItem<String>(value: e,child: Text(e))).toList();
+  }
+
+
+}
+
+enum Form{
+  WORD_COUNT_NEGATIVE,
+  WORD_COUNT_POSITIVE
 }
