eb54c012e9850917922c6a22dadfc56d9d338036
Nicola <nicolagabriele99@gmail.com>
2023-01-28 11:18:47 +0100

    Naive Besyan visulizzation

--- lib/Widgets/pages/ReviewsAutoClassificator.dart
+++ lib/Widgets/pages/ReviewsAutoClassificator.dart
@@ -7,9 +7,77 @@ class ReviewsAutoClassificator extends StatefulWidget{
 }
 
 class _ReviewsClassificatorState extends State<ReviewsAutoClassificator>{
+  String _recensione = "scrivi qui la tua recensione";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          title: Row(
+            children: [
+              Flexible(
+                child:Container(
+                    width: 75,
+                    height: 75,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle
+                    ) ,
+                    child: Image.asset("images/logo.jpg")
+                ),
+              ),
+              const Flexible(
+                child: Text(
+                  'Hotel Reviews Analitycs',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontStyle: FontStyle.italic
+                  ),
+                ),
+              ),
+
+            ],
+          )//aggiungere bellezze
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              color: Colors.green,
+              alignment: Alignment.center,
+              child: Text(
+                "Positive",
+                style: TextStyle(
+                  fontSize: 50
+                ),
+              ),
+            )
+          ),
+          Divider(color: Colors.black,height: 2,thickness: 2),
+           Padding(
+               padding:EdgeInsets.all(10),
+             child: TextField(
+               decoration: InputDecoration(
+                 label: Text(_recensione),
+               ),
+               onChanged: (value)=>_recensione = value,
+               maxLines: null,
+               keyboardType: TextInputType.multiline,
+             ),
+           ),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: ()=>{},
+                child: Text("submit"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith((states) => Colors.red)
+                ),
+              ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(color: Colors.red, elevation: 10,child: Container(height: 20,),), //valore base 20
+    );
   }
 
 }
